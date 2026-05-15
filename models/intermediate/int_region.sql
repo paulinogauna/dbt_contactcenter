afc731f5cf34c18de49b43a9290e4ad838dc46db
Paulino Gauna <paulino.gauna@perficient.com>
2026-05-15 20:51:59 +0000

    int_region

--- models/intermediate/int_region.sql
+++ models/intermediate/int_region.sql
@@ -1,18 +1,6 @@
-
-
 with region as (
-
-    select * from {{ ref('stg_dim_region') }}
-
+    select * from {{ ref('stg_dim_region') }}),
+region_actual as (
+{{ get_latest_records('region', ['region_id'], ['region_id', 'region_name'], 'loaded_at') }}
 )
-select group_region as (
-
-    select region_id,
-        region_name,
-        row_number() over (partition by region_id order by loaded_at desc) as rn
-        from region)
-
-select get_latest_records('group_region', ['region_id'], ['region_id', 'region_name'], 'loaded_at')
- 
-           
-
+select * from region_actual
